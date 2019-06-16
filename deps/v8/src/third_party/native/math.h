52d9008a88c845a84cf3998555baa966825d8b23
Amir Bawab <amirbawab@gmail.com>
2019-06-16 19:16:17 -0400

    Added dot product

--- deps/v8/src/third_party/native/math.h
+++ deps/v8/src/third_party/native/math.h
@@ -2,6 +2,7 @@
 #define V8_WASM_WASM_NATIVE_EXTERNAL_REFS_MATH_H_
 
 #define WASM_EXTERNAL_REFERENCE_MATH(F, V, P, R) \
-  V(F, sigmoid, "sigmoid_f64", P(kWasmF64), R(kWasmF64))
+  V(F, sigmoid, "sigmoid_f64", P(kWasmF64), R(kWasmF64)) \
+  V(F, dot_product_f32, "dot_product_f32", P(kWasmI32, kWasmI32, kWasmI32,kWasmI32, kWasmI32, kWasmI32), R())
 
 #endif // V8_WASM_WASM_NATIVE_EXTERNAL_REFS_MATH_H_
