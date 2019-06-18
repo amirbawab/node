7fff95f876dfa8b16a507a599b6c4c9227b98e55
Amir Bawab <amirbawab@gmail.com>
2019-06-17 20:05:36 -0400

    Added more dot product functions

--- deps/v8/src/third_party/native/math.cc
+++ deps/v8/src/third_party/native/math.cc
@@ -36,6 +36,50 @@ void dot_product_f32(Address mem, Address data) {
   Eigen::Map<Eigen::Matrix<float, Eigen::Dynamic, Eigen::Dynamic, Eigen::RowMajor>>(dst_p, lhs_row, rhs_col) = lhs * rhs;
 }
 
+void dot_product_rt_f32(Address mem, Address data) {
+  // Load parameters
+  int32_t* data_int32_p = reinterpret_cast<int32_t*>(data);
+  int32_t lhs_addr = ReadUnalignedValue<int32_t>(reinterpret_cast<Address>(data_int32_p++));
+  int32_t rhs_addr = ReadUnalignedValue<int32_t>(reinterpret_cast<Address>(data_int32_p++));
+  int32_t dst_addr = ReadUnalignedValue<int32_t>(reinterpret_cast<Address>(data_int32_p++));
+  int32_t lhs_row = ReadUnalignedValue<int32_t>(reinterpret_cast<Address>(data_int32_p++));
+  int32_t lhs_col = ReadUnalignedValue<int32_t>(reinterpret_cast<Address>(data_int32_p++));
+  int32_t rhs_col = ReadUnalignedValue<int32_t>(reinterpret_cast<Address>(data_int32_p));
+
+  // Locate buffers
+  byte* mem_byte_p = reinterpret_cast<byte*>(mem);
+  float* lhs_p = reinterpret_cast<float*>(mem_byte_p + lhs_addr);
+  float* rhs_p = reinterpret_cast<float*>(mem_byte_p + rhs_addr);
+  float* dst_p = reinterpret_cast<float*>(mem_byte_p + dst_addr);
+
+  // Perform dot product
+  auto lhs = Eigen::Map<Eigen::Matrix<float, Eigen::Dynamic, Eigen::Dynamic, Eigen::RowMajor>>(lhs_p, lhs_row, lhs_col);
+  auto rhs = Eigen::Map<Eigen::Matrix<float, Eigen::Dynamic, Eigen::Dynamic>>(rhs_p, lhs_col, rhs_col);
+  Eigen::Map<Eigen::Matrix<float, Eigen::Dynamic, Eigen::Dynamic, Eigen::RowMajor>>(dst_p, lhs_row, rhs_col) = lhs * rhs;
+}
+
+void dot_product_lt_f32(Address mem, Address data) {
+  // Load parameters
+  int32_t* data_int32_p = reinterpret_cast<int32_t*>(data);
+  int32_t lhs_addr = ReadUnalignedValue<int32_t>(reinterpret_cast<Address>(data_int32_p++));
+  int32_t rhs_addr = ReadUnalignedValue<int32_t>(reinterpret_cast<Address>(data_int32_p++));
+  int32_t dst_addr = ReadUnalignedValue<int32_t>(reinterpret_cast<Address>(data_int32_p++));
+  int32_t lhs_row = ReadUnalignedValue<int32_t>(reinterpret_cast<Address>(data_int32_p++));
+  int32_t lhs_col = ReadUnalignedValue<int32_t>(reinterpret_cast<Address>(data_int32_p++));
+  int32_t rhs_col = ReadUnalignedValue<int32_t>(reinterpret_cast<Address>(data_int32_p));
+
+  // Locate buffers
+  byte* mem_byte_p = reinterpret_cast<byte*>(mem);
+  float* lhs_p = reinterpret_cast<float*>(mem_byte_p + lhs_addr);
+  float* rhs_p = reinterpret_cast<float*>(mem_byte_p + rhs_addr);
+  float* dst_p = reinterpret_cast<float*>(mem_byte_p + dst_addr);
+
+  // Perform dot product
+  auto lhs = Eigen::Map<Eigen::Matrix<float, Eigen::Dynamic, Eigen::Dynamic>>(lhs_p, lhs_row, lhs_col);
+  auto rhs = Eigen::Map<Eigen::Matrix<float, Eigen::Dynamic, Eigen::Dynamic, Eigen::RowMajor>>(rhs_p, lhs_col, rhs_col);
+  Eigen::Map<Eigen::Matrix<float, Eigen::Dynamic, Eigen::Dynamic, Eigen::RowMajor>>(dst_p, lhs_row, rhs_col) = lhs * rhs;
+}
+
 }
 }
 }
