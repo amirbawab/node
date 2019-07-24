ac223a4f666e3e5dad8d810cff7702e83cb5cf86
Amir Bawab <amirbawab@gmail.com>
2019-07-23 21:39:02 -0400

    Added add example

--- deps/v8/src/third_party/native/math.cc
+++ deps/v8/src/third_party/native/math.cc
@@ -7,6 +7,13 @@ namespace v8 {
 namespace internal {
 namespace wasm {
 
+void add_i32(Address mem, Address data) {
+  int32_t* data_i32 = reinterpret_cast<int32_t*>(data);
+  auto l = ReadUnalignedValue<int32_t>(reinterpret_cast<Address>(data_i32));
+  auto r = ReadUnalignedValue<int32_t>(reinterpret_cast<Address>(data_i32+1));
+  WriteUnalignedValue<int32_t>(reinterpret_cast<Address>(data_i32+2), l + r);
+}
+
 void sigmoid(Address mem, Address data) {
   const double* res = reinterpret_cast<double*>(data) + 1;
   WriteUnalignedValue<double>(reinterpret_cast<Address>(res), 
--- deps/v8/src/third_party/native/math.h
+++ deps/v8/src/third_party/native/math.h
@@ -1,7 +1,8 @@
 #ifndef V8_WASM_WASM_NATIVE_EXTERNAL_REFS_MATH_H_
 #define V8_WASM_WASM_NATIVE_EXTERNAL_REFS_MATH_H_
 
-#define WASM_EXTERNAL_REFERENCE_MATH(F, V, P, R) \
+#define WASM_EXTERNAL_REFERENCE_MATH(F, V, P, R)               \
+  V(F, add_i32, "add_i32", P(kWasmI32, kWasmI32), R(kWasmI32)) \
   V(F, sigmoid, "sigmoid_f64", P(kWasmF64), R(kWasmF64))
 
-#endif // V8_WASM_WASM_NATIVE_EXTERNAL_REFS_MATH_H_
+#endif
